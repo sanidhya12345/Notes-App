644303651a9348fe28cfad6d0c1b4e0bc702de47
Sanidhya Varshney <sanidhya.varshney_cs19@gla.ac.in>
2023-11-02 16:27:22 +0530

    Update NoteApp.js

--- notesapp/src/NoteApp.js
+++ notesapp/src/NoteApp.js
@@ -2,19 +2,44 @@
 
 import React, { Component } from "react";
 import "./NoteApp.css";
+let notesArray=[];
+let comp;
+const Note = (props) => {
+  const style = {
+    backgroundColor: "#FF3",
+    width: "900px",
+    height: "100px",
+    marginTop:'20px',
+    border:'2px solid red'
+  };
+  return (<div style={style}>
+    <p>{props.data}</p>
+    <button type="button">Delete</button>
+  </div>);
+};
 class NoteApp extends Component {
   state = {
     note: "",
+    notesArray:[]
   };
-  handleNote=(e)=>{
-     const {name,value}=e.target;
-     this.setState({[name]:value});
-  }
-  clear=(e)=>{
+  handleNote = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  clear = (e) => {
     e.preventDefault();
-     this.setState({note:""});
+    this.setState({ note: "" });
+  };
+  save=(e)=>{
+      e.preventDefault();
+      notesArray.push(this.state.note);
+      this.setState({notesArray:notesArray})
+      comp=notesArray.map((data)=><Note data={data}></Note>)
   }
   render() {
+    const style={
+      display:'grid'
+    }
     return (
       <div>
         <h1 className="heading">Notes App</h1>
@@ -26,11 +51,19 @@ class NoteApp extends Component {
               cols="100"
               value={this.state.note}
               className="textarea"
-              placeholder="Write Note" onChange={this.handleNote}></textarea>
+              placeholder="Write Note"
+              onChange={this.handleNote}></textarea>
           </div>
           <div className="buttonSC">
-            <button type="button">Save</button>
-            <button type="button" onClick={this.clear}>Clear</button>
+            <button type="button" onClick={this.save}>Save</button>
+            <button type="button" onClick={this.clear}>
+              Clear
+            </button>
+          </div>
+        </div>
+        <div className="notes">
+          <div style={style}>
+            {comp}
           </div>
         </div>
       </div>
